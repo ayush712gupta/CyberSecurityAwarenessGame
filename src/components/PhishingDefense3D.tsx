2d543facb58fc5f0688f3c893dc524b48ad24ef5
Ayush Gupta <guptaayush8625@gmail.com>
2025-06-26 22:52:43 +0530

    Updated React code

--- src/components/PhishingDefense3D.tsx
+++ src/components/PhishingDefense3D.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const PhishingSimulator = () => {
   useEffect(() => {
-    window.location.href = "https://fuhadkalathingal.github.io/PhishGuard/level_1/level_1.html";
+    window.location.href = "https://ayush712gupta.github.io/PhishingAttack/level_1/level_1.html";
   }, []);
 
   return <p>Redirecting...</p>;
