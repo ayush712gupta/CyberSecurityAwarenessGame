1bf0e936961e464fe92469ad522fd4b1e61cf21f
Ayush Gupta <guptaayush8625@gmail.com>
2025-06-26 22:46:31 +0530

    Updated React app code and fixed router/config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CyberGuard/', // 👈 this must match your repo name
+  base: '/CyberSecurityAwarenessGame', // 👈 this must match your repo name
 })
